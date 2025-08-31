c791d87c81b016afcc2ca6dfcb1ee1951484bf3c
kirillbro99gd-collab <kirillbro99gd@gmail.com>
2025-08-31 13:28:52 +0300

    Update OnKeybindTrigger.hpp

--- src/OnKeybindTrigger.hpp
+++ src/OnKeybindTrigger.hpp
@@ -1,25 +1,27 @@
-// src/OnKeybindTrigger.hpp
 #pragma once
 #include <Geode/Geode.hpp>
-#include <Geode/modify/GameObject.hpp>
 #include <Geode/modify/EffectGameObject.hpp>
 
 using namespace geode::prelude;
 
+// Объявляем класс нашего кастомного триггера.
+// Он наследуется от EffectGameObject, как и большинство триггеров в игре.
 class OnKeybindTrigger : public EffectGameObject {
 public:
     // --- Переменные для хранения настроек ---
-    int m_targetGroupID = 0;
-    cocos2d::enumKeyCodes m_keyCode = KEY_None; // Какую клавишу нажимать
-    bool m_useCondition = false;                 // Использовать ли условие
-    int m_conditionItemID = 0;                   // ID предмета для условия
+    int m_targetGroupID;
+    cocos2d::enumKeyCodes m_keyCode;
+    bool m_useCondition;
+    int m_conditionItemID;
 
+    // Стандартные функции для создания объекта
     static OnKeybindTrigger* create();
-
     virtual bool init();
-    virtual void customObjectSetup(std::vector<std::string>&, std::vector<void*>&) override;
+
+    // Функции для сохранения и загрузки кастомных данных в уровень
+    virtual void customObjectSetup(std::vector<std::string>& fields, std::vector<void*>&) override;
     virtual std::string getSaveString(GJBaseGameLayer*) override;
     
-    // Функция для вызова меню настроек
-    virtual void buildSetup(GameObject*, std::vector<GameObject*>*) override;
+    // Функция, которая будет вызывать меню настроек
+    virtual void buildSetup(GameObject* obj, std::vector<GameObject*>* objs) override;
 };
